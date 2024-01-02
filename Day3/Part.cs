348254263d51ac5e5a7b8fe1be2aeddadaa38d03
servetvural <80199349+servetvural@users.noreply.github.com>
2024-01-02 19:38:46 +0000

    Add project files.

---
+++ Day3/Part.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3;
+public class Part
+{
+    public int value { get; set; }
+    public int row { get; set; }
+    public int colStart { get; set; }
+    public int colEnd
+    {
+        get
+        {
+            return colStart + value.ToString().Length-1;
+        }
+    }
+    public bool isAdjacent { get; set; }
+
+    public override string ToString()
+    {
+        return value + " " + isAdjacent + " r=" + row + " cs=" + colStart + " ce=" + colEnd;
+    }
+}
+public class Symbol
+{
+    public char symbol { get; set; }
+    public int row { get; set; }
+    public int col { get; set; }
+    
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,101 @@
+﻿using Day3;
+
+StreamReader reader = new StreamReader("input.txt");
+
+List<Part> parts = new List<Part>();
+List<Symbol> symbols = new List<Symbol>();
+int r = 0;
+while (true)
+{
+    String line = reader.ReadLine();
+    if (line == null)
+        break;
+
+    string numberAsString = "";
+    int colStart = 0;
+    for (int i = 0; i< line.Length; i++)
+    {
+        if (Char.IsDigit(line[i]))
+        {
+            //either found a first digit of a number or continuation of a number
+            if (numberAsString.Length > 0)
+            {   //it is continuation of a number so add
+                numberAsString += line[i];
+            } else
+            {   //it is first digit of a new number
+                numberAsString = line[i].ToString();
+                //this is the beginning of column
+                colStart = i;
+            }
+        } else
+        {   //something found but not a digit
+            //either you reached the end of number or not even started
+            if (numberAsString.Length > 0)
+            {
+                //there is a number already in process so finish the number
+                parts.Add(new Part() { 
+                    value = int.Parse(numberAsString),
+                    row = r,
+                    colStart = colStart
+                });
+                colStart = 0;
+                numberAsString = "";
+            } 
+            //process the symbol
+            if (line[i] == '.')
+            {
+                //nothing to do, it is a dot
+            } else
+            {
+                symbols.Add(new Symbol() { symbol= line[i], row = r, col = i });
+            }
+            
+        }
+
+    }
+    //increment row number        
+    r++;
+}
+
+
+bool test = Char.IsDigit('+');
+test = Char.IsDigit('=');
+test = Char.IsDigit('-');
+
+int total = 0;
+bool blnFound = false;
+int index = 0;
+foreach (Part part1 in parts)
+{
+    if (
+       //if there is a symbol on the same row and has column number 1 less than colstart or 1 more than colEnd of the part => there is a contact
+       symbols.Where(s => (s.row == part1.row && (s.col == part1.colStart - 1 || s.col == part1.colEnd + 1))  ||
+           //if there is a symbol on the previous row and has column number start from part col-1 to part col + 1
+           ( s.row == part1.row - 1 && s.col >= part1.colStart - 1 && s.col <= part1.colEnd + 1)    ||
+           //if there is a symbol on the next row and has column number start from part col-1 to part col + 1
+           ( s.row == part1.row + 1 && s.col >= part1.colStart - 1 && s.col <= part1.colEnd + 1))
+           .Any())
+    {
+        part1.isAdjacent = true;
+    }
+
+   // Console.WriteLine( part1.value + " " + blnFound + " i=" + index + " r=" + part1.row + " cs=" + part1.colStart + " ce=" + part1.colEnd);
+    index++;
+}
+
+Console.WriteLine(parts.Where(x => x.isAdjacent).Sum(x => x.value));
+/*
+foreach (Part part in parts) 
+    Console.WriteLine(part.value);
+
+foreach (Symbol symbol in symbols)
+    Console.WriteLine(symbol.symbol + " r=" + symbol.row + " c=" + symbol.col);
+*/
+
+
+
+Console.ReadLine();
+
+
+
+
